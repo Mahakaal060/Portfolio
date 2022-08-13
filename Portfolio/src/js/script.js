9259b5a9103cc049ec29073af21cc632698d7b08
Arjun Mishra <69663802+Mahakaal060@users.noreply.github.com>
2022-08-13 12:19:56 +0530

    Add files via upload

---
+++ Portfolio/src/js/script.js
@@ -0,0 +1,7 @@
+// ========== Typing Animation ==============
+var typed = new Typed(".typing", {
+  strings: ["Cyber Security student", "Web Developer", "Youtuber", "Gamer"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
